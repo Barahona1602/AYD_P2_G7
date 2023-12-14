fb2b96962b72b40137f9b69d1bbeab6a1558c132
Pablo <pbarahona07@gmail.com>
2023-12-14 12:02:44 -0600

    [add] environment backend with modules

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('¡Hola, mundo!');
-});
+//ACÁ VAN Las IMPORTACIONES DE LOS MÓDULOS
+const holaMundo = require('./modules/holaMundo');
+
+
+
+//ACÁ VAN LAS RUTAS
+app.use('/', holaMundo);
+
+
+
 
 app.listen(port, () => {
     console.log(`La aplicación está escuchando en http://localhost:${port}`);
