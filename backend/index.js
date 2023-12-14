5952613b5a710fd9d5f636d8a890d357ed90cf19
Pablo <pbarahona07@gmail.com>
2023-12-14 16:42:50 -0600

    [add] crud user

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ app.use(express.json());
 //ACÁ VAN Las IMPORTACIONES DE LOS MÓDULOS
 const holaMundo = require('./modules/holaMundo');
 const login = require('./modules/login');
+const crudUser = require('./modules/CRUDuser');
 
 
 
@@ -25,6 +26,7 @@ const login = require('./modules/login');
 //ACÁ VAN LAS RUTAS
 app.use('/', holaMundo);
 app.use('/', login);
+app.use('/', crudUser);
 
 
 
