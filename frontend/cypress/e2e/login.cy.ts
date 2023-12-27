c47cba9ba296699f5733c58c436ce8d6bda4571b
pereznator <3004272120101@ingenieria.usac.edu.gt>
2023-12-26 19:28:27 -0600

    test e2e de login implementado

--- frontend/cypress/e2e/login.cy.ts
+++ frontend/cypress/e2e/login.cy.ts
@@ -1,6 +1,3 @@
-import { LoginComponent } from "src/app/auth/login/login.component"
-// import { mount } from "cypress/angular"
-
 describe('login', () => {
   it('Deberia iniciar con los input vacios y el boton deshabilitado', () => {
     cy.visit("http://localhost:4200/auth/login");
@@ -9,12 +6,31 @@ describe('login', () => {
     cy.get("#password").should("be.empty");
   });
 
-  it('Debería de Dar error con contraseña invalida', () => {
+  it('Debería de dar error con correo y/o contraseña invalida', () => {
     cy.visit("http://localhost:4200/auth/login");
 
     cy.get("#email").type("cualquier.correo@gmail.com");
     cy.get("#password").type("contraseña invalida");
     cy.get("#btn").click();
+    cy.get("#alert")
+    .should("be.visible")
+    .invoke("text")
+    .then((text) => {
+      const trimmedText = text.trim();
+      expect(trimmedText).to.equal("Usuario o contraseña incorrectos");
+    });
   });
 
-})
+  it("Debería de ser exitoso si se ingresan datos válidos para el inicio de sesión y redirigir a la pantalla home", () => {
+    cy.visit("http://localhost:4200/auth/login");
+    cy.get("#email").type("jorge@gmail.com");
+    cy.get("#password").type("Pa$$word123");
+    cy.get("#btn").click();
+
+    // Espera a que se redirija a la ruta /home
+    cy.url().should('include', '/home');
+
+    // También puedes verificar elementos específicos en la página /home para asegurarte de que estás en la página correcta
+    cy.get("#titulo").should('have.text', 'Bienvenido Jorge Perez Ordóñez');
+  });
+});
